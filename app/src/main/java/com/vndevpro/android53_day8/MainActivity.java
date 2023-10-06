c9524bfc6bc5c3a6cce4126fa36f18cee57fe4bf
imthanhhai217@gmail.com <Haihai97@>
2023-10-06 21:05:56 +0700

    -add navigation - demo stream in collections

--- app/src/main/java/com/vndevpro/android53_day8/MainActivity.java
+++ app/src/main/java/com/vndevpro/android53_day8/MainActivity.java
@@ -1,19 +1,23 @@
 package com.vndevpro.android53_day8;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.Log;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 
-import com.google.gson.JsonObject;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,90 +25,67 @@ public class MainActivity extends AppCompatActivity {
     private Retrofit mRetrofit;
     private DummyServices dummyServices;
 
+    private BottomNavigationView bottom_nav;
+    private ArrayList<Product> mListData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mRetrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+//        mRetrofit = new Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+        initView();
+        loadListProduct();
 
-        dummyServices = mRetrofit.create(DummyServices.class);
+    }
+
+    private void initView() {
+        bottom_nav = findViewById(R.id.bottom_nav);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        NavigationUI.setupWithNavController(bottom_nav, navController);
+
+//        navController.navigate;
+        Bundle bundle = new Bundle();
+        bundle.putString("ID_PRODUCT", "1");
+        navController.navigate(R.id.action_homeFragment_to_productDetailsFragment, bundle);
+    }
+
+    private void loadListProduct() {
+        dummyServices = RetrofitClient.create(DummyServices.class);
 
         dummyServices.getProducts().enqueue(new Callback<ProductsResponse>() {
             @Override
             public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
-                if (response.isSuccessful()){
-                    if (response.code() == 200){
+                if (response.isSuccessful()) {
+                    if (response.code() == 200) {
                         ProductsResponse productsResponse = response.body();
-                        Log.d("TAG", "onResponse: "+productsResponse.getProducts().get(0).getTitle());
+                        Log.d("TAG", "onResponse: " + productsResponse.getProducts().get(0).getTitle());
                     }
                 }
             }
 
             @Override
             public void onFailure(Call<ProductsResponse> call, Throwable t) {
-                Log.d("TAG", "onFailure: "+t.getMessage());
+                Log.d("TAG", "onFailure: " + t.getMessage());
             }
         });
+    }
 
-        dummyServices.getProductById("2").enqueue(new Callback<Product>() {
-            @Override
-            public void onResponse(Call<Product> call, Response<Product> response) {
-                if (response.isSuccessful()){
-                    if (response.code() == 200){
-                        Product product = response.body();
+    public void demoStream() {
+        ArrayList<Product> hotDeals = (ArrayList<Product>) mListData.stream()
+                .filter(product -> product.getRating() > 4.0)
+                .collect(Collectors.toList());
 
-                        Log.d("TAG", "onResponse: getProductById : "+product.getTitle());
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Product> call, Throwable t) {
-
-            }
-        });
-
-        DummyServices dummyServices2 = RetrofitClient.create(DummyServices.class);
-
-        dummyServices2.getProducts().enqueue(new Callback<ProductsResponse>() {
-            @Override
-            public void onResponse(Call<ProductsResponse> call, Response<ProductsResponse> response) {
-
-            }
-
-            @Override
-            public void onFailure(Call<ProductsResponse> call, Throwable t) {
-
-            }
-        });
-        CartServices cartServices = RetrofitClient.create(CartServices.class);
+        ArrayList<Product> orderByPrice = (ArrayList<Product>) mListData.stream()
+                .sorted(Comparator.comparingDouble(Product::getPrice).reversed())
+                .collect(Collectors.toList());
 
 
-        dummyServices.addProduct(new AddProductRequest("Cái cốc")).enqueue(new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                Log.d("TAG", "onResponse: AddProductRequest "+response.body().get("id").getAsString());
-            }
-
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-                Log.d("TAG", "onFailure: ");
-            }
-        });
-        dummyServices.addProduct(new AddProductRequest("Cái cốc 2")).enqueue(new Callback<JsonObject>() {
-            @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                Log.d("TAG", "onResponse: AddProductRequest "+response.body().get("id").getAsString());
-            }
-
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-                Log.d("TAG", "onFailure: ");
-            }
-        });
     }
 }
